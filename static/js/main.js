baabf73863c127067ba9860e29568af173135dfd
btzr-io <btzr.io@gmail.com>
2017-04-07 01:11:50 -0600

    add hack to extract color palette

--- static/js/main.js
+++ static/js/main.js
@@ -3,31 +3,88 @@
 $(function(){
 
     var items = [],
-        username = "butterthemes",
+        username = "butterthemes/",
         api = "https://api.github.com/",
-        raw = "https://raw.githubusercontent.com/";
+        raw = "https://rawgit.com/"
+        raw2 = "https://raw.githack.com/";
 
-    function getPackageJson(user, repo, url, fn){
-        $.getJSON(raw + user + "/" + repo + "/master/package.json", function(json, status) {
-            if(status==="success"){
-                fn(json, url)
-            }
+        //Load css file....
+        function LoadCss(url){
+            var link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.type = "text/css";
+            link.href = url;
+            document.getElementsByTagName("head")[0].appendChild(link);
+        }
+
+        function getColors(theme, element){
+            element.classList.add(theme);
+            LoadCss(raw + username + theme + "/master/index.css");
+        }
+
+        function getPackageJson(user, repo, url, fn){
+            $.getJSON(raw + username + repo + "/master/package.json", function(json, status) {
+                if(status==="success") fn(json, url)
+            });
+        }
+
+        function CreateElement(opts){
+            var element = document.createElement(opts.type || 'div');
+                element.className = opts.class || '';
+                element.innerHTML = opts.content || '';
+                if(element.href) element.href = opts.href || '';
+                return element;
+        }
+
+        function Item(i, url) {
+
+        var element = CreateElement({
+            type: 'li',
+            class: 'item'
         });
-    }
 
-    function Item(i, url){
+        var header = CreateElement({
+            type: 'div',
+            class: 'header',
+            content: '<h3 class="title">'+ i.name +'</h3> <span class="version">'+i.version+'</span>',
+        });
 
-            var element = '<li class="item">' +
-                '<div class="header"><h3 class="title">'+ i.name +'</h3> <span class="version">'+i.version+'</span></div>' +
-                '<div class="description">'+i.description+ '</div>'+
-                '<div class="footer"><a class="button" href="'+url+'">Install</a></div>' +
-                '</li>';
+        element.appendChild(header);
 
-            $("#root .items").append(element);
+        var description = CreateElement({
+            type: 'div',
+            class: 'description',
+            content:  i.description
+        });
+
+        element.appendChild(description);
+
+        var color = '<li class="color"></li>';
+
+        var palette = CreateElement({
+            type: 'ul',
+            class: 'palette',
+            content: color + color + color
+        });
+
+        element.appendChild(palette);
+
+        var footer = CreateElement({
+            type: 'div',
+            class: 'footer',
+            content: '<a class="button" href="'+ url +'">Install</a>',
+            href: url
+        });
+
+        element.appendChild(footer);
+
+        getColors(i.name, palette);
+
+        $("#root .items").append(element);
     }
 
     //Load data
-    $.get(api + "orgs/" + username + "/repos", function(data, status){
+    $.get(api + "orgs/" + username + "repos", function(data, status){
         if(status==="success"){
             data.forEach( function (item, index) {
                 //Check for themes...
@@ -36,8 +93,6 @@ $(function(){
                     getPackageJson(item.owner.login, item.name, item.html_url, Item);
                 }
             });
-
         }
     });
-
 });
