807268e3a9aeb45fd11dab2520cbcbda542944e8
btzr-io <btzr.io@gmail.com>
2017-04-19 18:58:27 -0600

    add start count

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -3,18 +3,23 @@ import Colorpalette from '../color-palette';
 import style from './style.css';
 
 export default (props) => (
-        <div className={style.item}>
-            <Colorpalette colors={["#AAA",  "#BBB", "#CCC", "#DDD", "#EEE"]}/>
-            <div className={style.content}>
-                <h3 className={style.data}>
-                    <a className={style.name} href="?">{props.data.name}</a>
-                    <span className={style.version}>{props.data.version}</span>
-                </h3>
-                <p className={style.description}>{props.data.description}</p>
-            </div>
-            <div className={style.footer}>
-                <a className={style.user} href="#">{props.data.author}</a>
+
+    <div className={style.item}>
+        <link rel="stylesheet" type="text/css" href={props.data.url.css}/>
+        <Colorpalette colors={["#AAA",  "#BBB", "#CCC", "#DDD", "#EEE"]}/>
+        <div className={style.content}>
+            <h3 className={style.data}>
+                <a className={style.name} href="?">{props.data.name}</a>
+                <span className={style.version}>{props.data.version}</span>
+            </h3>
+            <p className={style.description}>{props.data.description}</p>
+        </div>
+        <div className={style.footer}>
+            <a className={style.user} href="#">{props.data.author}</a>
+            <div>
+                <span className={style.stars}>&#x2605; 0</span>
                 <a className={style.button} href="#">{"install"}</a>
             </div>
         </div>
-    )
+    </div>
+)
